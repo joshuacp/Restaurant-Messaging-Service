76589f565f2b77a30a91f2ce3b8ae39cb84e91f3
Josh_Patrick <joshpatrick88@gmail.com>
2014-04-18 19:32:32 -0600

    organization

---
+++ web/Scripts/Communication/Communication.js
@@ -0,0 +1,33 @@
+﻿function Communication() {}
+
+Communication.prototype.doPost = function (url2, data2, type2) {
+    console.log({
+        url: url2,
+        method: "POST",
+        data: data2,
+        async: 'true',
+        type: type2
+    });
+    $.ajax({
+        url: url2,
+        method: "POST",
+        data: data2,
+        async: 'true',
+        type: type2,
+        success: function (result) {
+            //tempproxy.getModel().loadDataFromJson(result);
+        }
+
+    });
+}
+
+Communication.prototype.doGet = function (url) {
+    var newModel;
+    $.ajax({
+        url: url,
+        type: 'GET',
+        async: 'true',
+        success: function (result) {
+        }
+    });
+}
